89790a111b8e147292c37c38548703f6594c347c
suzu <suzu.prg@gmail.com>
2021-04-29 01:36:59 +0900

    add validator of problem E

---
+++ E-Paper-Reading/tests/validator.cpp
@@ -0,0 +1,27 @@
+#include "constraints.h"
+#include "testlib.h"
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+void check() {
+  int N = inf.readInt(MIN_N, MAX_N_LARGE, "D");
+  inf.readEoln();
+  for (int i = 0; i < N; ++i) {
+    if (i != 0) inf.readSpace();
+    inf.readInt(MIN_T, MAX_T, "T_i");
+  }
+  inf.readEoln();
+}
+
+signed main(int argc, char *argv[]) {
+  registerValidation(argc, argv);
+  int T = inf.readInt(1, MAX_CASES, "T");
+  inf.readEoln();
+  for (int i = 0; i < T; i++) {
+    check();
+  }
+  inf.readEof();
+  return 0;
+}
