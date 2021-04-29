9b929f3e1d3909d1759014fedb02225055ac0495
emtsubasa <yagimeemeesann@gmail.com>
2021-04-29 23:39:47 +0900

    rename val

--- E-Paper-Reading/tests/generator.cpp
+++ E-Paper-Reading/tests/generator.cpp
@@ -5,19 +5,19 @@
 #include "testlib.h"
 using namespace std;
 
-void random(string fname, int cases, int minn, int maxn, int mint, int maxt);
+void random(string fname, int cases, int minn, int maxn, int mina, int maxa);
 
 int main(int argc, char* argv[]) {
   registerGen(argc, argv, 1);
-  random("small.in", MIN_CASES, MIN_N, MAX_N_SMALL, MIN_T, MAX_T);
-  random("large.in", MAX_CASES, MIN_N, MAX_N_LARGE, MIN_T, MAX_T);
+  random("small.in", MIN_CASES, MIN_N, MAX_N_SMALL, MIN_A, MAX_A);
+  random("large.in", MAX_CASES, MIN_N, MAX_N_LARGE, MIN_A, MAX_A);
   for (int i = 0; i < 10; ++i)
     random("random-test" + to_string(i) + ".in", MAX_CASES, MIN_N, MAX_N_LARGE,
-           MIN_T, MAX_T);
+           MIN_A, MAX_A);
   return 0;
 }
 
-void random(string fname, int cases, int minn, int maxn, int mint, int maxt) {
+void random(string fname, int cases, int minn, int maxn, int mina, int maxa) {
   ofstream of(fname);
   of << cases << endl;
   while (cases--) {
@@ -25,7 +25,7 @@ void random(string fname, int cases, int minn, int maxn, int mint, int maxt) {
     of << n << endl;
     for (int i = 0; i < n; ++i) {
       if (i != 0) of << " ";
-      of << rnd.next(mint, maxt);
+      of << rnd.next(mina, maxa);
     }
     of << endl;
   }
