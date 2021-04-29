6a0ca4498be0c0c11350bf7fa8639aff4ce8759d
suzu <suzu.prg@gmail.com>
2021-04-30 01:28:09 +0900

    modify solutions of problem E

--- E-Paper-Reading/Java-suzu-AC/Main.java
+++ E-Paper-Reading/Java-suzu-AC/Main.java
@@ -2,18 +2,18 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int test_case = sc.nextInt();
-        for(int test = 0; test < test_case; ++test) {
+        int T = sc.nextInt();
+        for(int test = 0; test < T; ++test) {
             int N = sc.nextInt();
-            int[] T = new int[N];
+            int[] A = new int[N];
             for (int i = 0; i < N; ++i) {
-                T[i] = sc.nextInt();
+                A[i] = sc.nextInt();
             }
             int min1 = Integer.MAX_VALUE;
             int min2 = Integer.MAX_VALUE;
             for(int i = 0; i < N; ++i){
-                if(T[i] < min2){
-                    min2 = T[i];
+                if(A[i] < min2){
+                    min2 = A[i];
                     if(min2 < min1){
                         int tmp = min2;
                         min2 = min1;
