0810cca32e087de1ef200445457d0af3e24183f7
Lgeu <nagiss000@gmail.com>
2021-05-08 06:12:52 +0900

    improve problem I TLE solution

--- I-The-Best-Base/Cpp-Lgeu-TLE/main.cpp
+++ I-The-Best-Base/Cpp-Lgeu-TLE/main.cpp
@@ -1,9 +1,13 @@
+#pragma GCC target("avx2")
+#pragma GCC optimize("O3")
+#pragma GCC optimize("unroll-loops")
+
 #include <array>
 using namespace std;
 
 constexpr array<int, 4> dys{0, 1, 0, -1};
 constexpr array<int, 4> dxs{1, 0, -1, 0};
-constexpr int max_N = 2048;
+constexpr int max_N = 1024;
 
 array<array<char, max_N>, max_N> A;
 array<array<bool, max_N>, max_N> searched;
@@ -21,6 +25,10 @@ void solve(){
         scanf("%d %d", &R, &C);
         A[R-1][C-1] = '*';
     }
+    if(Q==1) {
+        printf("0\n");
+        return;
+    }
     int answer = 1e9;
     for(int y_start = 0; y_start < N; y_start++){
         for(int x_start = 0; x_start < N; x_start++){
@@ -33,7 +41,7 @@ void solve(){
             for(int y = 0; y < N; y++) for(int x = 0; x < N; x++) searched[y][x] = false;
             searched[y_start][x_start] = true;
             if(A[y_start][x_start] == '*') visit_count++;
-            while(q_left != q_right){
+            while(q_left != q_right && sum_distances + (Q - visit_count) * (q_left->distance + 1) < answer){
                 for(int d = 0; d < 4; d++){
                     const int y = q_left->y + dys[d];
                     const int x = q_left->x + dxs[d];
@@ -42,7 +50,10 @@ void solve(){
                     if (A[y][x] == '*') {
                         visit_count++;
                         sum_distances += q_left->distance + 1;
-                        if(visit_count == Q) goto bb;
+                        if(visit_count == Q) {
+                            if(answer > sum_distances) answer = sum_distances;
+                            goto next_start;
+                        }
                     }
                     q_right->y = y;
                     q_right->x = x;
@@ -51,8 +62,7 @@ void solve(){
                 }
                 q_left++;
             }
-            bb:
-            if(answer > sum_distances) answer = sum_distances;
+            next_start:;
         }
     }
     printf("%d\n", answer);
