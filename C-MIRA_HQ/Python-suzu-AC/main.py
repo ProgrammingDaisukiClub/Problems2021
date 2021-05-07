0af6038ee8cfc922291630899769909da98fdf67
suzu <suzu.prg@gmail.com>
2021-05-07 13:47:59 +0900

    modify solution of problem C

--- C-MIRA_HQ/Python-suzu-AC/main.py
+++ C-MIRA_HQ/Python-suzu-AC/main.py
@@ -7,9 +7,12 @@ def main():
         N = int(input())
         L = list(map(int, input().split()))
         ans = 0
-        for i in L:
-            ans += i
-            print(ans)
+        for i in range(N):
+            if i != 0:
+                print(end=" ")
+            ans += L[i]
+            print(ans, end="")
+        print()
 
 
 if __name__ == '__main__':
