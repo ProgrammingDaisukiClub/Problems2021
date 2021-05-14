892d9e268b03a5b0b7d924e77effed3fe2f1a6ab
emtsubasa <yagimeemeesann@gmail.com>
2021-05-14 14:46:12 +0900

    rename problem G

--- C-MIRA_HQ/Cpp-tossy-AC/main.cpp
+++ C-MIRA_HQ/Cpp-tossy-AC/main.cpp
@@ -3,22 +3,22 @@
 using namespace std;
 
 void solve() {
-    int n;
-    cin>>n;
-    int cnt = 0;
-    for (int i=0; i<n; i++) {
-        int l;
-        cin>>l;
-        cnt += l;
-        cout << cnt << " \n"[i==n-1];
-    }
+  int n;
+  cin >> n;
+  int cnt = 0;
+  for (int i = 0; i < n; i++) {
+    int l;
+    cin >> l;
+    cnt += l;
+    cout << cnt << "\n";
+  }
 }
 
 int main() {
-    int t;
-    cin>>t;
+  int t;
+  cin >> t;
 
-    while(t-->0) solve();
+  while (t-- > 0) solve();
 
-    return 0;
+  return 0;
 }
