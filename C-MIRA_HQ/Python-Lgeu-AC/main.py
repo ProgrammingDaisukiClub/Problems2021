2658cd45a82684925670cfb41580d3c801d70336
Lgeu <nagiss000@gmail.com>
2021-05-14 20:10:31 +0900

    fix problem C Python-Lgeu-AC

--- C-MIRA_HQ/Python-Lgeu-AC/main.py
+++ C-MIRA_HQ/Python-Lgeu-AC/main.py
@@ -3,9 +3,7 @@ T = int(input())
 for _ in range(T):
     N = int(input())
     L = list(map(int, input().split()))
-    answer = []
     misw = 0
     for l in L:
         misw += l
-        answer.append(misw)
-    print(*answer)
+        print(misw)
